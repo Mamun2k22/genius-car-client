6ed6d247a00652d92f62b613410ae0945c0c84ee
Mamun2k22 <mamun180037@gmail.com>
2022-11-04 00:48:42 +0600

    Create slider, Gairdant overlay and Create about

---
+++ src/Pages/Home/Banner/BannerItem.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const BannerItem = ({ slide }) => {
+    const { image, id, prev, next } = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+            <div className='carousel-img'>
+                <img src={image} alt="" className="w-full rounded-xl" />
+            </div>
+
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                <h1 className='text-6xl font-bold text-white'>
+                    Awesome <br />
+                    Price for Car <br />
+                    Servicing
+                </h1>
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-24 w-2/5 top-1/2">
+                <p className='text-white text-xl'>
+                    There are many variations of passages Available! Here is many Cars!
+                </p>
+            </div>
+
+            <div className="absolute flex justify-start transform -translate-y-1/2 left-24 w-2/5 top-3/4">
+                <button className="btn btn-warning mr-5">Warning</button>
+                <button className="btn btn-warning">Success</button>
+
+            </div>
+
+
+
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href={`#slide ${prev}`} className="btn btn-circle mr-5">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+            </div>
+        </div>
+    );
+};
+
+export default BannerItem;
